4a26da00e8df7bec5ffade29012f26b4f88880ef
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2025-09-09 21:35:00 +0100

    stable vim config

---
+++ nvim/lua/oluwaseun/plugins-setup.lua
@@ -0,0 +1,156 @@
+-- auto install packer if not installed
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- import packer safely
+local status, packer = pcall(require, "packer")
+if not status then
+	return
+end
+
+-- add list of plugins to install
+return packer.startup(function(use)
+	-- packer can manage itself
+	use("wbthomason/packer.nvim")
+
+	-- Trouble
+	use({ "folke/trouble.nvim", opts = {}, cmd = "Trouble" })
+
+	use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
+
+	-- colorscheme
+	use("morhetz/gruvbox")
+	use("catppuccin/nvim")
+	use("rose-pine/neovim")
+
+	--Toggle term
+	use("akinsho/toggleterm.nvim")
+
+	use("szw/vim-maximizer") -- maximizes and restores current window
+
+	-- essential plugins
+	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
+	use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
+
+	--waka-time
+	use("wakatime/vim-wakatime")
+
+	--sooner(test)
+	--use("/home/oluwaseun/personal/sooner.nvim")
+
+	-- commenting with gc
+	use("numToStr/Comment.nvim")
+
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
+
+	-- vs-code like icons
+	use("nvim-tree/nvim-web-devicons")
+
+	-- statusline
+	use("nvim-lualine/lualine.nvim")
+
+	-- fuzzy finding w/ telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+
+	-- Go plugin
+	use("ray-x/go.nvim")
+
+	-- Flutter
+	-- use({
+	-- 	"nvim-flutter/flutter-tools.nvim",
+	-- 	requires = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"stevearc/dressing.nvim", -- optional for vim.ui.select
+	-- 	},
+	-- })
+
+	-- Code snippet
+	-- use({
+	-- 	"TobinPalmer/rayso.nvim",
+	-- 	cmd = { "Rayso" },
+	-- 	config = function()
+	-- 		require("rayso").setup({
+	-- 			open_cmd = "google-chrome",
+	-- 			options = {
+	-- 				title = "Oluwaseun",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- })
+	-- CodeSnap
+	use({ "mistricky/codesnap.nvim", run = "make" })
+
+	-- debugger (dap)
+	use({ "mfussenegger/nvim-dap", requires = { "leoluz/nvim-dap-go", "rcarriga/nvim-dap-ui" } })
+
+	use({ "nvim-neotest/nvim-nio" })
+
+	-- configuring lsp servers
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ -- Optional
+				"williamboman/mason.nvim",
+				run = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	})
+
+	-- enhanced lsp uis
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+
+	-- treesitter configuration
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
+
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+
+	-- git integration
+	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
+end)
